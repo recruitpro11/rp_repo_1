90b84854a2bcbab0ffa2dd902ebc9b4e32cbe5fe
hooman67 <houman_sh2001@hotmail.com>
2017-03-06 18:45:18 +0000

    adding xperts catch first page

--- experssUserLogin/models/user.js
+++ experssUserLogin/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var db = mongoose.connection;
+//var bcrypt = require('bcrypt');
 
 mongoose.connect('mongodb://35.163.48.45/xpertscatch');
 
@@ -36,5 +37,6 @@ var User = module.exports = mongoose.model('User', UserSchema);
 //Start the functions you want this model to handle with module.exports
 //so that they are availabel to us outside this file
 module.exports.createUser = function(newUser, callback){
+  //      bcrypt.hash(newUser.password,10,function(){}{});
 	newUser.save(callback);
 }
