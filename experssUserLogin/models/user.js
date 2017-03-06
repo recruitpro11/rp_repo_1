4cf11093b38935b464b8fe6bdfc4239d1ab27080
hooman67 <houman_sh2001@hotmail.com>
2017-03-06 20:16:09 +0000

    added bcrypt

--- experssUserLogin/models/user.js
+++ experssUserLogin/models/user.js
@@ -1,19 +1,25 @@
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-//var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 
 mongoose.connect('mongodb://35.163.48.45/xpertscatch');
 
 
 //User Schema
 var UserSchema = mongoose.Schema({
+        occupation: {
+                type: String,
+                index: true
+        },
 	username: {
 		type: String,
 		index:  true
 	},
 
 	password: {
-		type: String
+		type: String, 
+                required: true,
+                bcrypt: true
 	},
 
 	email: {
@@ -37,6 +43,11 @@ var User = module.exports = mongoose.model('User', UserSchema);
 //Start the functions you want this model to handle with module.exports
 //so that they are availabel to us outside this file
 module.exports.createUser = function(newUser, callback){
-  //      bcrypt.hash(newUser.password,10,function(){}{});
-	newUser.save(callback);
+        bcrypt.hash(newUser.password,10,function(err, hash){
+          if(err) throw err;
+          //set hashed pw
+          newUser.password = hash
+          //put user in db
+          newUser.save(callback);
+        });
 }
--- experssUserLogin/routes/users.js
+++ experssUserLogin/routes/users.js
@@ -18,10 +18,33 @@ router.get('/register', function(req, res, next) {
   });
 });
 
+/* GET localhost:3000/users/register/prof */
+router.get('/register/prof', function(req, res, next) {
+  res.render('profRegForm', {
+    'title':'profReg'
+  });
+});
+
+/* GET localhost:3000/users/register/ta */
+router.get('/register/ta', function(req, res, next) {
+  res.render('taRegForm', {
+    'title':'taReg'
+  });
+});
+
+/* GET localhost:3000/users/register/hm */
+router.get('/register/hm', function(req, res, next) {
+  res.render('hmRegForm', {
+    'title':'hmReg'
+  });
+});
+
+
 /* POST localhost:3000/users/login */
-router.post('/register', function(req, res, next){
+router.post('/register/prof', function(req, res, next){
 
   //get form values.  req.body.name  === the name of the input in   input.form-control  in register.jade file 
+  var formOccupation = "prof";
   var formName = req.body.name;
   var formEmail = req.body.email;
   var formUsername = req.body.username;
@@ -76,6 +99,7 @@ router.post('/register', function(req, res, next){
 
     //Instantiate new user. Using User class which we define
     var newUser = new User({
+      occupation: formOccupation,
       name : formName,
       email : formEmail,
       username: formUsername,
@@ -104,6 +128,187 @@ router.post('/register', function(req, res, next){
  
 
 });
+
+
+
+/* POST localhost:3000/users/register/ta */
+router.post('/register/ta', function(req, res, next){
+
+  //get form values.  req.body.name  === the name of the input in   input.form-control  in register.jade file
+  var formOccupation = "ta";
+  var formName = req.body.name;
+  var formEmail = req.body.email;
+  var formUsername = req.body.username;
+  var formPassword = req.body.password;
+  var formPassword2 = req.body.password2;
+
+//console.log("this is req:\n")
+//console.log(req.body)
+//console.log("\n\n")
+
+  //Check for Image Field
+  if(req.files && req.files.profileimage){
+
+    console.log("Uploading File...");
+
+    //file info
+    var formProfileImageOriginalName = req.files.profileimage.originalname;
+    var formProfileImageName         = req.files.profileimage.name
+    var formProfileImageMime         = req.files.profileimage.mimetype;
+    var formProfileImagePath         = req.files.profileimage.path
+    var formProfileImageExtension    = req.files.profileimage.extension;
+    var formProfileImageSize         = req.files.profileimage.size
+  } else {
+    // Set a default profile image. We put this in the uploads folder ourselves
+    var formProfileImageName = 'noImage.png';
+  }
+
+
+  //Validate the Form
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email not valid').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+
+  //store the errors for rendering
+  var formErrors = req.validationErrors();
+//console.log(formErrors)
+  if(formErrors){
+
+    res.render('register',{
+      errors : formErrors,
+      name : formName,
+      email : formEmail,
+      username: formUsername,
+      password: formPassword,
+      password2: formPassword2
+    });
+  } else {
+    //Instantiate new user. Using User class which we define
+    var newUser = new User({
+      occupation: formOccupation,
+      name : formName,
+      email : formEmail,
+      username: formUsername,
+      password: formPassword,
+      password2: formPassword2,
+      profileimage: formProfileImageName
+    });
+
+//console.log("this is new user: \n")
+//console.log(newUser)
+
+    //Create user
+    User.createUser(newUser, function(err,user){
+      if(err) throw err;
+      console.log(user);
+    });
+
+    //Send a success flash message
+    req.flash('success','You are now registered and may login');
+
+    //redirect them to them homePage
+    res.location('/');
+    res.redirect('/');
+
+  }
+
+
+});
+
+
+
+/* POST localhost:3000/users/register/hm */
+router.post('/register/hm', function(req, res, next){
+
+  //get form values.  req.body.name  === the name of the input in   input.form-control  in register.jade file
+  var formOccupation = "hm";
+  var formName = req.body.name;
+  var formEmail = req.body.email;
+  var formUsername = req.body.username;
+  var formPassword = req.body.password;
+  var formPassword2 = req.body.password2;
+
+//console.log("this is req:\n")
+//console.log(req.body)
+//console.log("\n\n")
+
+  //Check for Image Field
+  if(req.files && req.files.profileimage){
+
+    console.log("Uploading File...");
+
+    //file info
+    var formProfileImageOriginalName = req.files.profileimage.originalname;
+    var formProfileImageName         = req.files.profileimage.name
+    var formProfileImageMime         = req.files.profileimage.mimetype;
+    var formProfileImagePath         = req.files.profileimage.path
+    var formProfileImageExtension    = req.files.profileimage.extension;
+    var formProfileImageSize         = req.files.profileimage.size
+  } else {
+    // Set a default profile image. We put this in the uploads folder ourselves
+    var formProfileImageName = 'noImage.png';
+  }
+
+
+  //Validate the Form
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email not valid').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+
+  //store the errors for rendering
+  var formErrors = req.validationErrors();
+//console.log(formErrors)
+  if(formErrors){
+
+    res.render('register',{
+      errors : formErrors,
+      name : formName,
+      email : formEmail,
+      username: formUsername,
+      password: formPassword,
+      password2: formPassword2
+    });
+  } else {
+    //Instantiate new user. Using User class which we define
+    var newUser = new User({
+      occupation: formOccupation,
+      name : formName,
+      email : formEmail,
+      username: formUsername,
+      password: formPassword,
+      password2: formPassword2,
+      profileimage: formProfileImageName
+    });
+
+//console.log("this is new user: \n")
+//console.log(newUser)
+
+    //Create user
+    User.createUser(newUser, function(err,user){
+      if(err) throw err;
+      console.log(user);
+    });
+
+    //Send a success flash message
+    req.flash('success','You are now registered and may login');
+
+    //redirect them to them homePage
+    res.location('/');
+    res.redirect('/');
+
+  }
+
+
+});
+
 
 
 
