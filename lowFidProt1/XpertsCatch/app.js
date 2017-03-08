17df2c6f2a647f96a18bdd3d881f0b932f8b9ca8
hooman67 <houman_sh2001@hotmail.com>
2017-03-08 00:31:22 +0000

    adding some files

--- lowFidProt1/XpertsCatch/app.js
+++ lowFidProt1/XpertsCatch/app.js
@@ -2,10 +2,25 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
+//hs middleware declarations
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
+
+//routes
+//index is the main page user sees after login
 var index = require('./routes/index');
+//userse is where we put auth stuff register/ login etc.
 var users = require('./routes/users');
 
 var app = express();
@@ -14,14 +29,72 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//hs handles file uploads
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//hs handles express sessions secret can be anything you want
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//ha passport must be after express session middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//hs validators we wanna have error handling here
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
 app.use(cookieParser());
+
+//public folder is where we'll put all static pages
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+/* index and users are locaions of local directories
+
+
+//routes
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+Here we're telling the app to use index location as the root for our website 
+
+Basically: on website: /     === ./routes/index   on server  
+and        on website: /users === ./routes/users   on server
+
+*/
 app.use('/', index);
 app.use('/users', users);
 
