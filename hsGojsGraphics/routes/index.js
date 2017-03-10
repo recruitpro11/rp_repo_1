d27b5ba24bc978256a2595538c342b3a38d7af07
hooman67 <houman_sh2001@hotmail.com>
2017-03-10 01:31:03 +0000

    adding cLayout example

--- hsGojsGraphics/routes/index.js
+++ hsGojsGraphics/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hs', { title: 'ejs' });
+  res.render('cLayout', { title: 'ejs' });
 });
 
 module.exports = router;
