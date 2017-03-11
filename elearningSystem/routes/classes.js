cf107e7552ffd02e31d9b0ff82563bb027d03f14
hooman67 <houman_sh2001@hotmail.com>
2017-03-11 21:01:45 +0000

    final commit: all working

--- elearningSystem/routes/classes.js
+++ elearningSystem/routes/classes.js
@@ -44,6 +44,39 @@ console.log(classname);
 });
 
 
+router.get('/:id/lessons/:lesson_nb', ensureAuthenticated, function(req, res, next) {
+   
+  //get the class, and look through its lessons for the one 
+  //we are looking for
+  Class.getClassById(req.params.id, function(err, classname){
+console.log('hss got classname:\n');
+console.log(classname);
+    var lesson;
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      for(i=0; i<classname.lessons.length; i++){
+console.log('hss lesson:\n');
+console.log(classname.lessons[i]);
+        if(classname.lessons[i].lesson_number == req.params.lesson_nb){
+console.log('hs found  the lesson we were looking for');
+           lesson = classname.lessons[i];
+        }
+      }
+      res.render('classes/lesson', {'class': classname, "lesson":lesson });
+    }
+  });
+});
+
+
+function ensureAuthenticated(req, res, next){
+  //this is passports authentication API
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/');
+}
+
 
 module.exports = router;
-
