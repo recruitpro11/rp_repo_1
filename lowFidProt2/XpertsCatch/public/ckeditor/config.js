662c94d80af12b3d381f6295a2e4897b8dbe1d25
hooman67 <houman_sh2001@hotmail.com>
2017-03-13 20:31:31 +0000

    Got rid of ckeditor's <p> tags

--- lowFidProt2/XpertsCatch/public/ckeditor/config.js
+++ lowFidProt2/XpertsCatch/public/ckeditor/config.js
@@ -35,4 +35,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+        config.enterMode = CKEDITOR.ENTER_BR // pressing the ENTER KEY input <br/>
+        config.shiftEnterMode = CKEDITOR.ENTER_P; //pressing the SHIFT + ENTER KEYS input <p>
+        config.autoParagraph = false; // stops automatic insertion of <p> on focus
 };
