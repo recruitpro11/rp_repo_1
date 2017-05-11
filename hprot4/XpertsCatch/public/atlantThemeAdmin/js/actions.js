6ab8e0e1d9a89366d22aea9e3b75a1e26f0cfce8
hooman67 <houman_sh2001@hotmail.com>
2017-05-11 00:57:08 +0000

    collapsable list first commit

--- hprot4/XpertsCatch/public/atlantThemeAdmin/js/actions.js
+++ hprot4/XpertsCatch/public/atlantThemeAdmin/js/actions.js
@@ -245,6 +245,41 @@ var page_actions = function(){
     });
     /* EOF ACCORDION */
 
+
+
+ /* HS ACCORDION */
+    $(".accordion .hs a").on("click",function(){
+
+        var blockOpen = $(this).attr("href");
+        var accordion = $(this).parents(".accordion");        
+        var noCollapse = accordion.hasClass("accordion-dc");
+
+
+        if($(blockOpen).length > 0){            
+
+            if($(blockOpen).hasClass("panel-body-open")){
+                $(blockOpen).slideUp(200,function(){
+                    $(this).removeClass("panel-body-open");
+                });
+            }else{
+                $(blockOpen).slideDown(200,function(){
+                    $(this).addClass("panel-body-open");
+                });
+            }
+
+            if(!noCollapse){
+                accordion.find(".panel-body-open").not(blockOpen).slideUp(200,function(){
+                    $(this).removeClass("panel-body-open");
+                });                                           
+            }
+
+            return false;
+        }
+
+    });
+    /* Ez HS ACCORDION */
+
+
     /* DATATABLES/CONTENT HEIGHT FIX */
     $(".dataTables_length select").on("change",function(){
         onresize();
